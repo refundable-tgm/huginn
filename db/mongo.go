8ebf61a2e6562ac93a0013fa10914f5bbcd7e089
Michael Beier <admin@mbeier.at>
2021-02-28 21:08:30 +0100

    created some methods to interact with applications in the database

--- db/mongo.go
+++ db/mongo.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"context"
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"io/ioutil"
@@ -10,16 +12,26 @@ import (
 	"time"
 )
 
+// The name of the collection in which the Teacher data is stored in
 const TeacherCollection = "Teacher"
+
+// The name of the collection in which the Application data is stored in
 const ApplicationCollection = "Collection"
 
+// Containing data used for the mongo db connection
 type MongoDatabaseConnector struct {
+	// the name of the database in the mongo db server
 	database string
-	client   *mongo.Client
-	context  context.Context
-	closer   context.CancelFunc
+	// the client used in this connection
+	client *mongo.Client
+	// the created context of the client
+	context context.Context
+	// CancelFunc of the context
+	closer context.CancelFunc
 }
 
+// Connects the MongoDatabaseConnector with the given MongoDB server
+// returns whether this was successful
 func (m *MongoDatabaseConnector) Connect() bool {
 	uri, db, ok := resolveURI()
 	if ok {
@@ -44,6 +56,8 @@ func (m *MongoDatabaseConnector) Connect() bool {
 	return false
 }
 
+// Closes the Connection to the MongoDB
+// returns whether this operation was successful
 func (m MongoDatabaseConnector) Close() (ok bool) {
 	err := m.client.Disconnect(m.context)
 	m.closer()
@@ -54,19 +68,63 @@ func (m MongoDatabaseConnector) Close() (ok bool) {
 	return true
 }
 
-func (m MongoDatabaseConnector) CreateApplication() (ok bool) {
-	return false
+// Creates a new application in the system
+func (m MongoDatabaseConnector) CreateApplication(application Application) bool {
+	application.uuid = uuid.New().String()
+	collection := m.client.Database(m.database).Collection(ApplicationCollection)
+	insert, err := collection.InsertOne(m.context, application)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	log.Println("Inserted a new application with the UUID: ", application.uuid,
+		"; the Title: ", application.name, "; under the ID: ", insert.InsertedID)
+	return true
 }
 
-func (m MongoDatabaseConnector) GetApplication() (application Application) {
-	return Application{}
+// Returns a specific application described by its uuid
+func (m MongoDatabaseConnector) GetApplication(uuid string) (application Application) {
+	collection := m.client.Database(m.database).Collection(ApplicationCollection)
+	if err := collection.FindOne(m.context, bson.M{"uuid": uuid}).Decode(&application); err != nil {
+		log.Println(err)
+		return
+	}
+	return application
+}
+
+// Returns all active applications in the system
+func (m MongoDatabaseConnector) GetActiveApplications() (applications []Application) {
+	filter := bson.M{
+		"$or": []bson.M{
+			{"$and": []bson.M{
+				{"kind": Training},
+				{"progress": bson.M{"$in": []int{TRejected, TInProcess, TConfirmed, TRunning, TCostsPending, TCostsInProcess}}},
+			}},
+			{"$and": []bson.M{
+				{"kind": SchoolEvent},
+				{"progress": bson.M{"$in": []int{SERejected, SEInSubmission, SEInProcess, SEConfirmed, SERunning, SECostsPending, SECostsInProcess}}},
+			}},
+		},
+	}
+	collection :=  m.client.Database(m.database).Collection(ApplicationCollection)
+	cursor, err := collection.Find(m.context, filter)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = cursor.All(m.context, &applications)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	return applications
 }
 
 func (m MongoDatabaseConnector) UpdateApplication() (ok bool) {
 	return false
 }
 
-func (m MongoDatabaseConnector) DeleteApplication() (ok bool) {
+func (m MongoDatabaseConnector) DeleteApplication(uuid string) (ok bool) {
 	return false
 }
 
@@ -86,6 +144,9 @@ func (m MongoDatabaseConnector) DeleteTeacher() (ok bool) {
 	return false
 }
 
+// Constructs the URI out of the given information of the docker secrets
+// returns the constructed URI, the database name, and whether the operation was successful
+// if it was not successful the URI and the database name are empty strings
 func resolveURI() (URI string, database string, ok bool) {
 	database = os.Getenv("MONGO_DATABASE")
 	usernameFilePath := os.Getenv("MONGO_USERNAME_FILE")
