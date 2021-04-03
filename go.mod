84ae7cba3a4cd3fc31663a3ee163072dde5be6b2
Michael Beier <admin@mbeier.at>
2021-04-03 21:04:31 +0200

    configuring rest service for tokens

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/refundable-tgm/huginn
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.6.3
 	github.com/google/uuid v1.2.0
 	go.mongodb.org/mongo-driver v1.4.6
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
--- rest/rest.go
+++ rest/rest.go
@@ -1,24 +1,17 @@
 package rest
 
 import (
+	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
 	"strconv"
 )
 
 const Port = 8080
 
 func StartService() {
-	log.Printf("%d endpoints were registered", registerEndpoints(handlers))
-	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(Port), nil))
-}
-
-func registerEndpoints(points []endpoint) int {
-	var amount int
-	var p endpoint
-	for amount, p = range points {
-		http.HandleFunc(p.path, p.handler)
-		log.Printf("Registered #%d for path '%v'", amount + 1, p.path)
-	}
-	return amount + 1
+	InitTokenManager()
+	router := gin.Default()
+	//gin.SetMode(gin.ReleaseMode)
+	router.POST("/login", Login)
+	log.Fatal(router.Run(":" + strconv.Itoa(Port)))
 }
