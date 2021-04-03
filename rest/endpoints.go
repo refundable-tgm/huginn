043cfbddeb6d7044bdde713cf463f08238d7e245
Michael Beier <admin@mbeier.at>
2021-04-03 21:04:47 +0200

    created login endpoint

--- rest/endpoints.go
+++ rest/endpoints.go
@@ -1,17 +1,33 @@
 package rest
 
-import "net/http"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/refundable-tgm/huginn/ldap"
+	"net/http"
+)
 
-type endpoint struct {
-	path    string
-	handler func(writer http.ResponseWriter, request *http.Request)
-}
-
-var handlers = []endpoint{
-	{
-		"/login",
-		func(w http.ResponseWriter, r *http.Request) {
-
-		},
-	},
+func Login(con *gin.Context) {
+	u := struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}{}
+	if err := con.ShouldBindJSON(&u); err != nil {
+		con.JSON(http.StatusUnprocessableEntity, "invalid request structure provided")
+		return
+	}
+	if !ldap.AuthenticateUserCredentials(u.Username, u.Password) {
+		con.JSON(http.StatusUnauthorized, "this credentials do not resolve into an authorized login")
+		return
+	}
+	token, err := CreateToken(u.Username)
+	if err != nil {
+		con.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	SaveToken(u.Username, token)
+	out := map[string]string{
+		"access_token": token.AccessToken,
+		"refresh_token": token.RefreshToken,
+	}
+	con.JSON(http.StatusOK, out)
 }
